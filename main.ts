8bdbab439bfe08a1c195690ab37fa890f7ba032b
Nicolás Flores Alvear <146827258+NicolasFlores7e7@users.noreply.github.com>
2024-10-24 17:20:22 +0200

    Repte 1, acabado

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
-basic.forever(function () {
-	
+let mano = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.SmallSquare)
+})
+input.onGesture(Gesture.Shake, function () {
+    mano = randint(0, 2)
+    if (mano == 0) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else if (mano == 1) {
+        basic.showIcon(IconNames.Square)
+    } else if (mano == 2) {
+        basic.showIcon(IconNames.Scissors)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Scissors)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Square)
 })
